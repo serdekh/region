6d37d1f8e5759af359113cfa9bbf67e1dff08787
Sergij Dekht <mistersergey055@gmail.com>
2026-03-13 19:09:19 +0100

    Add test cases for the __region_alloc function

--- tests/main.c
+++ tests/main.c
@@ -211,6 +211,7 @@ TestContext testfn__region_alloc_case_0()
     
     if (context.error.code == REGION_ERROR_TYPE_NOT_ENOUGH_MEMORY) {
         LOG_TEST_PASSED("__region_alloc", 0);
+        if (region) free(region);
         return context;
     }
     
@@ -219,7 +220,32 @@ TestContext testfn__region_alloc_case_0()
         error_code_to_str(context.error.code));
         
     if (region) free(region);
+    context.passed = false;
     
+    return context;
+}
+
+TestContext testfn__region_alloc_case_1()
+{
+    TestContext context = get_test_context(0, TESTOUT_FILE_PATH);
+    
+    Region *region = region_alloc(0, &context.error);
+    
+    const char *expected = "[Region][ERROR](<TEST_FILE_NAME>:0:<TEST_FUNC_NAME>()): Invalid arguments. The `region` cannot have `capacity` equal to zero.\n";
+
+    __region_log_error(context.error, context.out_stream);
+
+    char *actual = get_str_from_file(context.out_stream);
+
+    if (strcmp(expected, actual) != 0) {
+        LOG_TEST_FAILED("__region_alloc", 1, expected, actual);
+        free(actual);
+        context.passed = false;
+        return context;
+    }
+    
+    free(actual);
+    LOG_TEST_PASSED("__region_alloc", 1);
     return context;
 }
 
@@ -283,6 +309,32 @@ TestContext testfn__region_alloc_item_case_1()
     return context;
 }
 
+TestContext testfn__region_alloc_item_case_2()
+{
+    TestContext context = get_test_context(0, TESTOUT_FILE_PATH);
+
+    Region *region = region_alloc(1, &context.error);
+
+    if (!region) {
+        fprintf(stderr, "[Region][Test][Error]: Couldn't allocate memory for testing. Stop.\n");
+        exit(1);
+    }
+
+    region_alloc_item(region, 10, &context.error);
+
+    if (region->next) {
+        LOG_TEST_PASSED("__region_alloc_item", 2);
+        region_free(&region);
+        return context;
+    }
+
+    LOG_TEST_FAILED("__region_alloc_item", 2, "The value of the `next` field not to be NULL due to excessive capacity", "Nullable reference");
+    context.passed = false;
+    region_free(&region);
+    return context;
+
+}
+
 bool testfn__region_alloc(TestContext *context)
 {   
     *context = testfn__region_alloc_case_0(); if (!context->passed) goto failed;
@@ -299,6 +351,7 @@ bool testfn__region_alloc_item(TestContext *context)
 {   
     *context = testfn__region_alloc_item_case_0(); if (!context->passed) goto failed;
     *context = testfn__region_alloc_item_case_1(); if (!context->passed) goto failed;
+    *context = testfn__region_alloc_item_case_2(); if (!context->passed) goto failed;
 
     fprintf(stderr, "[Region][Test][Passed]: for the function `__region_alloc_item`\n");
     return context->passed;
@@ -316,7 +369,7 @@ int main()
     if (!testfn__region_log_error(&context)) goto exiting;
     if (!testfn__region_alloc(&context)) goto exiting;
     if (!testfn__region_alloc_item(&context)) goto exiting;
-    
+
     printf("[Region][Test]: All tests have passed!\n");
     return 0;
 
