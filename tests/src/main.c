11e6c5aadf9e936a5cd00b189114ce20bcfe6aec
Sergij Dekht <mistersergey055@gmail.com>
2026-03-16 21:32:06 +0100

    Rewrite the makefile to print the valgrind's output only if any errors have occured

--- tests/src/main.c
+++ tests/src/main.c
@@ -4,15 +4,15 @@ int main()
 {
     TestContext context = {0};
 
-    if (!testfn__region_log_error(&context)) goto exiting;
-    if (!testfn__region_alloc(&context)) goto exiting;
-    if (!testfn__region_alloc_item(&context)) goto exiting;
-    if (!testfn__region_free(&context)) goto exiting;
+    if (!testfn__region_log_error(&context))  goto fatal;
+    if (!testfn__region_alloc(&context))      goto fatal;
+    if (!testfn__region_alloc_item(&context)) goto fatal;
+    if (!testfn__region_free(&context))       goto fatal;
 
-    printf("[Region][Test]: All tests have passed!\n");
+    printf("[Region][Test]: All function tests have passed!\n");
     return 0;
 
-exiting:
+fatal:
     if (context.out_stream) fclose(context.out_stream);
     return (int)context.passed;
 }
