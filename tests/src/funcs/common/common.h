d022d720d087222d7c4d906bbb9cde5288d58959
Sergij Dekht <mistersergey055@gmail.com>
2026-04-02 17:53:00 +0200

    Add a symbol name a definition for the '__stack_region_push' function

--- tests/src/funcs/common/common.h
+++ tests/src/funcs/common/common.h
@@ -11,6 +11,7 @@
 #define SYMBOL_FN_REGION_FREE "region_free"
 
 #define SYMBOL_FN_STACK_REGION_ALLOC "__stack_region_alloc"
+#define SYMBOL_FN_STACK_REGION_PUSH "__stack_region_push"
 
 #define SYMBOL_FN_TEST_SET_AVAILABLE_MEMORY "__test_set_available_memory"
 #define SYMBOL_FN_TEST_GET_AVAILABLE_MEMORY "__test_get_available_memory"
@@ -21,6 +22,7 @@ typedef void (*FuncPtr_region_shrink_capacity)(Region *region, RegionError *erro
 typedef void (*FuncPtr_region_free)(Region **region);
 
 typedef StackRegion *(*FuncPtr_stack_region_alloc)(size_t capacity, RegionError *error, RegionLocation location);
+typedef void *(*FuncPtr_stack_region_push)(StackRegion *stack, size_t size, RegionError *error, RegionLocation location);
 
 typedef void (*FuncPtr_test_set_available_memory)(size_t value);
 typedef size_t (*FuncPtr_test_get_available_memory)();
