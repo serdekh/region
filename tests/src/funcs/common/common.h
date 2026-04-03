729a3da46c4da4b7cf8170084bba7d854de2b19a
Sergij Dekht <mistersergey055@gmail.com>
2026-04-03 20:09:55 +0200

    Add a defintion and a symbol name for the '__stack_region_pop' function

--- tests/src/funcs/common/common.h
+++ tests/src/funcs/common/common.h
@@ -13,6 +13,7 @@
 #define SYMBOL_FN_STACK_REGION_ALLOC "__stack_region_alloc"
 #define SYMBOL_FN_STACK_REGION_PUSH "__stack_region_push"
 #define SYMBOL_FN_STACK_REGION_PEEK "__stack_region_peek"
+#define SYMBOL_FN_STACK_REGION_POP "__stack_region_pop"
 
 #define SYMBOL_FN_TEST_SET_AVAILABLE_MEMORY "__test_set_available_memory"
 #define SYMBOL_FN_TEST_GET_AVAILABLE_MEMORY "__test_get_available_memory"
@@ -25,6 +26,7 @@ typedef void (*FuncPtr_region_free)(Region **region);
 typedef StackRegion *(*FuncPtr_stack_region_alloc)(size_t capacity, RegionError *error, RegionLocation location);
 typedef void *(*FuncPtr_stack_region_push)(StackRegion *stack, size_t size, RegionError *error, RegionLocation location);
 typedef void *(*FuncPtr_stack_region_peek)(StackRegion *stack, RegionError *error, RegionLocation location);
+typedef void *(*FuncPtr_stack_region_pop)(StackRegion *stack, RegionError *error, RegionLocation location);
 
 typedef void (*FuncPtr_test_set_available_memory)(size_t value);
 typedef size_t (*FuncPtr_test_get_available_memory)();
