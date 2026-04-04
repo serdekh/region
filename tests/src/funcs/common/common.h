d5588fca18ffb4fc9238fd95a9ad9e2b4ce064c0
Sergij Dekht <mistersergey055@gmail.com>
2026-04-04 12:44:46 +0200

    Add a definition and a symbol name for the 'region_get_last_node' function

--- tests/src/funcs/common/common.h
+++ tests/src/funcs/common/common.h
@@ -9,6 +9,7 @@
 #define SYMBOL_FN_REGION_PUSH "__region_push"
 #define SYMBOL_FN_REGION_SHRINK_CAPACITY "__region_shrink_capacity"
 #define SYMBOL_FN_REGION_FREE "region_free"
+#define SYMBOL_FN_REGION_GET_LAST_NODE "region_get_last_node"
 
 #define SYMBOL_FN_STACK_REGION_ALLOC "__stack_region_alloc"
 #define SYMBOL_FN_STACK_REGION_PUSH "__stack_region_push"
@@ -22,6 +23,7 @@ typedef Region*(*FuncPtr_region_alloc)(size_t, RegionError*, RegionLocation);
 typedef void *(*FuncPtr_region_push)(Region *region, size_t size, RegionError *error, RegionLocation location);
 typedef void (*FuncPtr_region_shrink_capacity)(Region *region, RegionError *error, RegionLocation location);
 typedef void (*FuncPtr_region_free)(Region **region);
+typedef Region *(*FuncPtr_region_get_last_node)(Region *region, RegionError *error, RegionLocation location);
 
 typedef StackRegion *(*FuncPtr_stack_region_alloc)(size_t capacity, RegionError *error, RegionLocation location);
 typedef void *(*FuncPtr_stack_region_push)(StackRegion *stack, size_t size, RegionError *error, RegionLocation location);
