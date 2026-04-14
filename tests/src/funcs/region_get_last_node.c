44a380209531ec58b366c52bd72a87b296d9793a
Sergij Dekht <mistersergey055@gmail.com>
2026-04-14 08:42:17 +0200

    Add a case to test a new option

--- tests/src/funcs/region_get_last_node.c
+++ tests/src/funcs/region_get_last_node.c
@@ -2,12 +2,19 @@
 
 FuncPtr_region_get_last_node fn = NULL;
 
+FuncPtr_region_free  lib_region_free   = NULL;
+FuncPtr_region_alloc lib_region_alloc  = NULL;
+
 #define TEST_REGION_GET_LAST_NODE_CAPACITY 1
 
 void try_init_test_fn()
 {
     if (!_RegionHandle) try_get_region_handle();
+
     if (!fn) fn = try_get_symbol(SYMBOL_FN_REGION_GET_LAST_NODE);
+
+    if (!lib_region_free)  lib_region_free  = try_get_symbol(SYMBOL_FN_REGION_FREE);
+    if (!lib_region_alloc) lib_region_alloc = try_get_symbol(SYMBOL_FN_REGION_ALLOC);
 }
 
 TestResult test_region_get_last_node_case_1()
@@ -15,7 +22,7 @@ TestResult test_region_get_last_node_case_1()
     try_init_test_fn();
 
     TestResult result = {0};
-    RegionError error = {0};
+    RegionError error = {0}; REGION_ERROR_INIT_LOCATION(&error);
 
     fn(NULL, REGION_GET_LAST_NODE_OPTION_DEFAULT, &error);
 
@@ -24,29 +31,41 @@ TestResult test_region_get_last_node_case_1()
     return result;
 }
 
+Region *_alloc_region_with_two_nodes(RegionError *error)
+{
+    Region *first = NULL;
+    Region *second = NULL;
+
+    first = lib_region_alloc(TEST_REGION_GET_LAST_NODE_CAPACITY, error); 
+
+    if (!first) return NULL;
+
+    second = lib_region_alloc(TEST_REGION_GET_LAST_NODE_CAPACITY, error); 
+
+    if (!second) {
+        lib_region_free(&first);
+        return NULL;
+    }
+
+    first->next = second;
+
+    return first;
+}
+
 TestResult test_region_get_last_node_case_2()
 {
     try_init_test_fn();
     
     TestResult result = {0};
-    RegionError error = {0};
+    RegionError error = {0}; REGION_ERROR_INIT_LOCATION(&error);
 
-    Region *first = NULL;
-    Region *second = NULL;
+    Region *region_with_two_nodes = _alloc_region_with_two_nodes(&error); UNWRAP;
 
-    FuncPtr_region_free  region_free   = try_get_symbol(SYMBOL_FN_REGION_FREE);
-    FuncPtr_region_alloc region_alloc  = try_get_symbol(SYMBOL_FN_REGION_ALLOC);
+    Region *last_node = fn(region_with_two_nodes, REGION_GET_LAST_NODE_OPTION_DEFAULT, &error); UNWRAP;
 
-    first  = region_alloc(TEST_REGION_GET_LAST_NODE_CAPACITY, &error); UNWRAP;
-    second = region_alloc(TEST_REGION_GET_LAST_NODE_CAPACITY, &error); UNWRAP;
+    TEST_RESULT_WRITE_PTR(result, region_with_two_nodes->next, last_node);
 
-    first->next = second;
-
-    Region *last_node = fn(first, REGION_GET_LAST_NODE_OPTION_DEFAULT, &error); UNWRAP;
-
-    TEST_RESULT_WRITE_PTR(result, second, last_node);
-
-    region_free(&first);
+    lib_region_free(&region_with_two_nodes);
     
     return result;
 
@@ -54,7 +73,36 @@ fatal:
     REGION_LOG_ERROR(error);
     fprintf(stderr, "[Test][Error]: Failed to perform a test for the `region_get_last_node` function. Stop.\n");
 
-    if (first) region_free(&first);
+    if (region_with_two_nodes) lib_region_free(&region_with_two_nodes);
+    if (_RegionHandle) dlclose(_RegionHandle);
+
+    exit(1);
+}
+
+TestResult test_region_get_last_node_case_3()
+{
+    try_init_test_fn();
+    
+    TestResult result = {0};
+    RegionError error = {0}; REGION_ERROR_INIT_LOCATION(&error);
+
+    Region *region_with_two_nodes = _alloc_region_with_two_nodes(&error); UNWRAP;
+
+    region_with_two_nodes->next->size = 0;
+
+    Region *last_node = fn(region_with_two_nodes, REGION_GET_LAST_NODE_OPTION_NON_EMPTY, &error); UNWRAP;
+
+    TEST_RESULT_WRITE_PTR(result, region_with_two_nodes, last_node);
+
+    lib_region_free(&region_with_two_nodes);
+    
+    return result;
+
+fatal:
+    REGION_LOG_ERROR(error);
+    fprintf(stderr, "[Test][Error]: Failed to perform a test for the `region_get_last_node` function. Stop.\n");
+
+    if (region_with_two_nodes) lib_region_free(&region_with_two_nodes);
     if (_RegionHandle) dlclose(_RegionHandle);
 
     exit(1);
@@ -62,5 +110,6 @@ fatal:
 
 REGISTER_TEST(test_region_get_last_node_case_1, 1);
 REGISTER_TEST(test_region_get_last_node_case_2, 2);
+REGISTER_TEST(test_region_get_last_node_case_3, 3);
 
 EXPOSE(tests);
