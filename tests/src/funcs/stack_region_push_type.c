bc5614a2e29707da5f8f91bc29b206212964fa02
Sergij Dekht <mistersergey055@gmail.com>
2026-04-19 20:52:58 +0200

    Add tests for pushing numeric types onto the stack region

---
+++ tests/src/funcs/stack_region_push_type.c
@@ -0,0 +1,137 @@
+#include "../include/shared.h"
+
+#define TEST_STACK_REGION_PUSH_TYPE_CASE2_STATIC_DATA "TEST_STACK_REGION_PUSH_TYPE_CASE2_STATIC_DATA"
+#define TEST_STACK_REGION_PUSH_TYPE_CASE2_STATIC_DATA_LENGTH sizeof(TEST_STACK_REGION_PUSH_TYPE_CASE2_STATIC_DATA)
+#define TEST_STACK_REGION_PUSH_TYPE_CASE3_VALUE 123
+#define TEST_STACK_REGION_PUSH_TYPE_CASE4_VALUE 456
+
+#define stack_region_push_type(type, stack, value, error) stack_region_push_##type(stack, value, error)
+
+#define TEST_STACK_REGION_PUSH_TYPE_CASE_1(type, expected_error_code)   \
+    TestResult test_stack_region_push_## type ##_case_1(RegionAPI *api) \
+    {                                                                   \
+        TestResult result = {0};                                        \
+                                                                        \
+        RegionError error = REGION_ERROR_INIT;                          \
+                                                                        \
+        api->stack_region_push_##type(NULL, 0, &error);                 \
+                                                                        \
+        TEST_RESULT_WRITE_INT(result, expected_error_code, error.code); \
+                                                                        \
+        return result;                                                  \
+    }                                                                   \
+
+#define TEST_STACK_REGION_PUSH_TYPE_CASE_2(type, expected_error_code)          \
+    TestResult test_stack_region_push_## type ##_case_2(RegionAPI *api)        \
+    {                                                                          \
+        TestResult result = {0};                                               \
+                                                                               \
+        RegionError error = REGION_ERROR_INIT;                                 \
+                                                                               \
+        StackRegion stack = {                                                  \
+            .next     = NULL,                                                  \
+            .data     = TEST_STACK_REGION_PUSH_TYPE_CASE2_STATIC_DATA,         \
+            .capacity = TEST_STACK_REGION_PUSH_TYPE_CASE2_STATIC_DATA_LENGTH,  \
+            .size     = TEST_STACK_REGION_PUSH_TYPE_CASE2_STATIC_DATA_LENGTH   \
+        };                                                                     \
+                                                                               \
+        api->test_set_available_memory(0);                                     \
+            api->stack_region_push_type(type, &stack, 0, &error);              \
+        api->test_set_available_memory(REGION_TEST_AVAILABLE_MEMORY_DEFAULT);  \
+                                                                               \
+        TEST_RESULT_WRITE_INT(result, expected_error_code, error.code);        \
+                                                                               \
+        return result;                                                         \
+    }                                                                          \
+
+#define TEST_STACK_REGION_PUSH_TYPE_CASE_3(type, fmt)                                                             \
+    TestResult test_stack_region_push_## type ##_case_3(RegionAPI *api)                                           \
+    {                                                                                                             \
+        TestResult result = {0};                                                                                  \
+                                                                                                                  \
+        RegionError error = REGION_ERROR_INIT;                                                                    \
+                                                                                                                  \
+        StackRegion *stack = NULL;                                                                                \
+                                                                                                                  \
+        stack = api->stack_region_alloc(sizeof(type) + sizeof(size_t), &error); UNWRAP;                           \
+                                                                                                                  \
+        type *pushed = api->stack_region_push_type(type, stack, TEST_STACK_REGION_PUSH_TYPE_CASE3_VALUE, &error); \
+                                                                                                                  \
+        type *peeked = (type *)(stack->data);                                                                     \
+                                                                                                                  \
+        if (pushed != peeked) {                                                                                   \
+            TEST_RESULT_WRITE_PTR(result, pushed, peeked);                                                        \
+            api->stack_region_free(&stack);                                                                       \
+            return result;                                                                                        \
+        }                                                                                                         \
+                                                                                                                  \
+        TEST_RESULT_WRITE_FMT(result, fmt, *pushed, *peeked);                                                     \
+                                                                                                                  \
+        api->stack_region_free(&stack);                                                                           \
+                                                                                                                  \
+        return result;                                                                                            \
+                                                                                                                  \
+        TEST_FATAL(if (stack) api->stack_region_free(&stack));                                                    \
+    }                                                                                                             \
+
+#define TEST_STACK_REGION_PUSH_TYPE_CASE_4(type, fmt)                                                             \
+    TestResult test_stack_region_push_## type ##_case_4(RegionAPI *api)                                           \
+    {                                                                                                             \
+        TestResult result = {0};                                                                                  \
+                                                                                                                  \
+        RegionError error = REGION_ERROR_INIT;                                                                    \
+                                                                                                                  \
+        StackRegion *stack = NULL;                                                                                \
+                                                                                                                  \
+        stack = api->stack_region_alloc(sizeof(type) / 2, &error); UNWRAP;                                        \
+                                                                                                                  \
+        type *pushed = api->stack_region_push_type(type, stack, TEST_STACK_REGION_PUSH_TYPE_CASE4_VALUE, &error); \
+                                                                                                                  \
+        type *peeked = (type *)(stack->next->data);                                                               \
+                                                                                                                  \
+        if (pushed != peeked) {                                                                                   \
+            TEST_RESULT_WRITE_PTR(result, pushed, peeked);                                                        \
+            api->stack_region_free(&stack);                                                                       \
+            return result;                                                                                        \
+        }                                                                                                         \
+                                                                                                                  \
+        TEST_RESULT_WRITE_FMT(result, fmt, *pushed, *peeked);                                                     \
+                                                                                                                  \
+        api->stack_region_free(&stack);                                                                           \
+                                                                                                                  \
+        return result;                                                                                            \
+                                                                                                                  \
+        TEST_FATAL(if (stack) api->stack_region_free(&stack));                                                    \
+    }                                                                                                             \
+
+TEST_STACK_REGION_PUSH_TYPE_CASE_1(int, REGION_ERROR_CODE_EINVAL_STACK_REGION_PUSH_INT_NO_STACK_REGION);
+TEST_STACK_REGION_PUSH_TYPE_CASE_2(int, REGION_ERROR_CODE_ENOMEM_STACK_REGION_PUSH_INT_MALLOC_REGION);
+TEST_STACK_REGION_PUSH_TYPE_CASE_3(int, "%d");
+TEST_STACK_REGION_PUSH_TYPE_CASE_4(int, "%d");
+
+TEST_STACK_REGION_PUSH_TYPE_CASE_1(float, REGION_ERROR_CODE_EINVAL_STACK_REGION_PUSH_FLOAT_NO_STACK_REGION);
+TEST_STACK_REGION_PUSH_TYPE_CASE_2(float, REGION_ERROR_CODE_ENOMEM_STACK_REGION_PUSH_FLOAT_MALLOC_REGION);
+TEST_STACK_REGION_PUSH_TYPE_CASE_3(float, "%f");
+TEST_STACK_REGION_PUSH_TYPE_CASE_4(float, "%f");
+
+TEST_STACK_REGION_PUSH_TYPE_CASE_1(double, REGION_ERROR_CODE_EINVAL_STACK_REGION_PUSH_DOUBLE_NO_STACK_REGION);
+TEST_STACK_REGION_PUSH_TYPE_CASE_2(double, REGION_ERROR_CODE_ENOMEM_STACK_REGION_PUSH_DOUBLE_MALLOC_REGION);
+TEST_STACK_REGION_PUSH_TYPE_CASE_3(double, "%f");
+TEST_STACK_REGION_PUSH_TYPE_CASE_4(double, "%f");
+
+REGISTER_TEST(test_stack_region_push_int_case_1, 1);
+REGISTER_TEST(test_stack_region_push_int_case_2, 2);
+REGISTER_TEST(test_stack_region_push_int_case_3, 3);
+REGISTER_TEST(test_stack_region_push_int_case_4, 4);
+
+REGISTER_TEST(test_stack_region_push_float_case_1, 1);
+REGISTER_TEST(test_stack_region_push_float_case_2, 2);
+REGISTER_TEST(test_stack_region_push_float_case_3, 3);
+REGISTER_TEST(test_stack_region_push_float_case_4, 4);
+
+REGISTER_TEST(test_stack_region_push_double_case_1, 1);
+REGISTER_TEST(test_stack_region_push_double_case_2, 2);
+REGISTER_TEST(test_stack_region_push_double_case_3, 3);
+REGISTER_TEST(test_stack_region_push_double_case_4, 4);
+
+EXPORT_AT_TESTS_SECTION;
