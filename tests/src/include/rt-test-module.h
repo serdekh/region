040c4be1d62bdcbc76354ec45bc4e16daf3d4506
Sergij Dekht <mistersergey055@gmail.com>
2026-04-25 17:02:14 +0200

    cosmetic patch

--- tests/src/include/rt-test-module.h
+++ tests/src/include/rt-test-module.h
@@ -8,25 +8,25 @@
 #define RT_GET_TEST_MODULE_END        _rt_get_test_module_end
 #define RT_GET_TEST_MODULE_END_STR   "_rt_get_test_module_end"
 
-#define RT_TEST_MODULE_EXPORT                                           \
-    extern const TestContext __start_rt_test_module[];    \
-    extern const TestContext __stop_rt_test_module[];      \
-                                                                        \
-    __attribute__((visibility("default")))                              \
+#define RT_TEST_MODULE_EXPORT                           \
+    extern const TestContext __start_rt_test_module[];  \
+    extern const TestContext __stop_rt_test_module[];   \
+                                                        \
+    __attribute__((visibility("default")))              \
     const TestContext *RT_GET_TEST_MODULE_START(void) { \
-        return __start_rt_test_module;                    \
-    }                                                                   \
-                                                                        \
-    __attribute__((visibility("default")))                              \
-    const TestContext *RT_GET_TEST_MODULE_END(void) {  \
-        return __stop_rt_test_module;                      \
+        return __start_rt_test_module;                  \
+    }                                                   \
+                                                        \
+    __attribute__((visibility("default")))              \
+    const TestContext *RT_GET_TEST_MODULE_END(void) {   \
+        return __stop_rt_test_module;                   \
     }
 
-#define RT_TEST_MODULE_REGISTER(f, num)                                             \
-    static const TestContext __rt_target_##f                                               \
-                                                                                    \
-    __attribute__((section("rt_test_module"), used, aligned(sizeof(void*)))) = {        \
-        .case_number = num,                                                         \
-        .func_name = #f,                                                            \
-        .func = f,                                                                  \
+#define RT_TEST_MODULE_REGISTER(f, num)                                          \
+    static const TestContext __rt_target_##f                                     \
+                                                                                 \
+    __attribute__((section("rt_test_module"), used, aligned(sizeof(void*)))) = { \
+        .case_number = num,                                                      \
+        .func_name = #f,                                                         \
+        .func = f,                                                               \
     };                        
