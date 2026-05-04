98be7eec7a874bdfa5d0b410017233ab8fba8fd2
serdekh <mistersergey055@gmail.com>
2026-05-04 19:50:38 +0200

    Fix the missing symbol errors on Windows

--- tests/src/lib/region-test.c
+++ tests/src/lib/region-test.c
@@ -4,29 +4,43 @@
 #define REGION_TEST_AVAILABLE_MEMORY_DEFAULT 2048
 
 void *test_malloc(size_t capacity);
-void __test_set_available_memory(size_t value);
-void __test_set_default_memory();
-size_t __test_get_available_memory();
+
+#if defined(_WIN32)
+    #define TEST_API __declspec(dllexport)
+#else
+    #define TEST_API __attribute__((visibility("default")))
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+TEST_API void __test_set_available_memory(size_t value);
+TEST_API void __test_set_default_memory(void);
+TEST_API size_t __test_get_available_memory(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #define REGION_MALLOC test_malloc
 #define REGION_FREE free
-
 #define REGION_IMPLEMENTATION
 #include "../../../src/region.h"
 
 static size_t __test_available_memory = REGION_TEST_AVAILABLE_MEMORY_DEFAULT;
 
-void __test_set_available_memory(size_t value) 
+TEST_API void __test_set_available_memory(size_t value) 
 {
     __test_available_memory = value;
 }
 
-void __test_set_default_memory() 
+TEST_API void __test_set_default_memory(void) 
 {
     __test_available_memory = REGION_TEST_AVAILABLE_MEMORY_DEFAULT;
 }
 
-size_t __test_get_available_memory()
+TEST_API size_t __test_get_available_memory(void)
 {
     return __test_available_memory;
 }
@@ -35,6 +49,7 @@ void *test_malloc(size_t capacity)
 {
     if (capacity > __test_available_memory) return NULL;
 
+    // overflow-safe check
     if (__test_available_memory - capacity > __test_available_memory) {
         __test_available_memory = 0;
         return NULL;
