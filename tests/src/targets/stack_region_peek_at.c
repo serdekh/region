429499016c4da0a2c7dc0fbce6246033b382a43f
Sergij Dekht <mistersergey055@gmail.com>
2026-05-13 20:21:15 +0200

    Add a target that catches a new error log

--- tests/src/targets/stack_region_peek_at.c
+++ tests/src/targets/stack_region_peek_at.c
@@ -92,9 +92,35 @@ TestResult test_stack_region_peek_at_case_4(RegionAPI *api)
     RT_TARGET_FATAL_ERROR(if (stack) api->stack_region_free(&stack););
 }
 
+
+TestResult test_stack_region_peek_at_case_5(RegionAPI *api)
+{
+    TestResult result = {0};
+
+    RegionError error = REGION_ERROR_INIT;
+
+    StackRegion *stack = NULL;
+
+    stack = api->stack_region_alloc(1, &error); RT_TARGET_UNWRAP;
+
+    stack->size = 1;
+    *(size_t *)(stack->data - sizeof(size_t)) = 1;
+
+    api->stack_region_peek_at(stack, 0, &error);
+
+    RT_TEST_RESULT_WRITE_INT(result, REGION_ERROR_MESSAGE_CORRUPTED_STACK_REGION_DATA, error.message);
+
+    api->stack_region_free(&stack);
+
+    return result;
+
+    RT_TARGET_FATAL_ERROR(if (stack) api->stack_region_free(&stack));
+}
+
 RT_TEST_MODULE_REGISTER(test_stack_region_peek_at_case_1, 1);
 RT_TEST_MODULE_REGISTER(test_stack_region_peek_at_case_2, 2);
 RT_TEST_MODULE_REGISTER(test_stack_region_peek_at_case_3, 3);
 RT_TEST_MODULE_REGISTER(test_stack_region_peek_at_case_4, 4);
+RT_TEST_MODULE_REGISTER(test_stack_region_peek_at_case_5, 5);
 
 RT_TEST_MODULE_EXPORT;
